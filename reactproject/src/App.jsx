1637866cbf7079eb73cdb409108008ca142dd427
poojitha20-rs <vennapoojita20@gmail.com>
2024-10-18 11:48:16 +0530

    first commit

---
+++ reactproject/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+// import {useState} from 'react'
+// import reactlogo from './assets/react.svg'
+// import vitelogo from './vite.svg'
+// import './App.css'
+function App() {
+  // const{const.setConst}
+  return (
+    <>
+    <h1>App</h1>
+    </>
+  )
+}
+
+export default App
