02ac130c86a3244b94bca9277e58fb7ae7eaaa46
poojitha20-rs <vennapoojita20@gmail.com>
2024-10-18 11:51:36 +0530

    files3

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react"
-// import {useState} from 'react'
-// import reactlogo from './assets/react.svg'
-// import vitelogo from './vite.svg'
-// import './App.css'
+import {useState} from 'react'
+ import reactlogo from './assets/react.svg'
+import vitelogo from './vite.svg'
+import './App.css'
 function App() {
-  // const{const.setConst}
+   const{count,setCount}=webState(0)
   return (
     <>
-    <h1>App</h1>
+    <h1>This is tkr college</h1>
+    <h2>You are beautiful</h2>
     </>
   )
 }
